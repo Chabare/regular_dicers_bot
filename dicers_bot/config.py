9e262170be013529ab04023b6a3a9b078cf87d69
chabare <chabare95@gmail.com>
2019-03-04 19:09:39 +0100

    Catch exception while opening/parsing config.json

--- dicers_bot/config.py
+++ dicers_bot/config.py
@@ -1,10 +1,22 @@
 import json
+from json import JSONDecodeError
+
+from dicers_bot.logger import create_logger
 
 
 class Config(dict):
     def __init__(self, filename: str, **kwargs):
-        with open(filename, "r") as file:
-            content = json.load(file)
-            self.update(content)
+        logger = create_logger("config")
+        try:
+            logger.debug(f"Open {filename}")
+            with open(filename, "r") as file:
+                logger.debug("Load file content as json")
+                content = json.load(file)
+                create_logger("Update config")
+                self.update(content)
+        except OSError:
+            logger.error(f"Couldn't open {filename} due to an OS error", exc_info=True)
+        except JSONDecodeError:
+            logger.error(f"Couldn't open {filename} due to json decoding error", exc_info=True)
 
         super().__init__(**kwargs)
