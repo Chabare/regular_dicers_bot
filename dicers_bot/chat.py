60ed4b14ba0a8b8bdb1ba77d3b5c7444eceaa2dd
Torben Carstens <github@carstens.tech>
2019-03-22 10:06:48 +0100

    Use set comprehension

--- dicers_bot/chat.py
+++ dicers_bot/chat.py
@@ -63,7 +63,7 @@ class Chat:
         chat.pinned_message_id = json_object["pinned_message_id"]
         chat.current_event = Event.deserialize(json_object["current_event"])
         chat.events = [Event.deserialize(event_json_object) for event_json_object in json_object["events"]]
-        chat.users = set([User.deserialize(user_json_object) for user_json_object in json_object.get("users", [])])
+        chat.users = {User.deserialize(user_json_object) for user_json_object in json_object.get("users", [])}
 
         return chat
 
