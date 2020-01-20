5e7288a227a1f3ac4668b4da34f8fada623c48aa
Torben Carstens <github@carstens.tech>
2020-01-20 21:06:52 +0100

    Add ingredients for cocktails
    [skip ci]

--- dicers_bot/cocktails.py
+++ dicers_bot/cocktails.py
@@ -7,24 +7,39 @@ from graphqlclient import GraphQLClient
 
 
 @dataclass
+class Ingredient:
+    name: str
+
+    @classmethod
+    def from_dict(cls, json_element: Dict) -> "Ingredient":
+        return cls(json_element.get("name"))
+
+    def __str__(self):
+        return self.name
+
+
+@dataclass
 class Cocktail:
     name: str
     jumbo: bool
     alcoholic: bool
+    ingredients: List[Ingredient]
 
     @classmethod
     def from_dict(cls, json_element: Dict) -> "Cocktail":
-        return Cocktail(json_element.get("name"), json_element.get("jumbo"), json_element.get("alcoholic"))
+        ingredients = [Ingredient.from_dict(d) for d in json_element.get("ingredients")]
+        return cls(json_element.get("name"), json_element.get("jumbo"), json_element.get("alcoholic"), ingredients)
 
     def __str__(self) -> str:
         jumbo = "(Jumbo)" if self.jumbo else ""
         alcoholic = "💔" if not self.alcoholic else ""
+        ingredients = "(" + ", ".join(map(str, self.ingredients)) + ")"
 
-        return " ".join([self.name, jumbo, alcoholic])
+        return " ".join([self.name, jumbo, alcoholic, ingredients])
 
 
 def get_cocktails() -> Optional[List[Cocktail]]:
-    client = GraphQLClient('https://rd-backend.carstens.tech/graphql')
+    client = GraphQLClient('http://localhost:8000/graphql')  # TODO
 
     i = '''
     {
@@ -32,6 +47,9 @@ def get_cocktails() -> Optional[List[Cocktail]]:
         name
         jumbo
         alcoholic
+        ingredients {
+            name
+        }
       }
     }
     '''
